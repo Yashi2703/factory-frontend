0e52622c1df3ca13d801a40a067a622cbe3016ef
yashi <yashig26@gmail.com>
2025-01-20 22:06:36 +0530

    worked on orders

--- src/Components/AddOrder.js
+++ src/Components/AddOrder.js
@@ -1,8 +1,137 @@
-import { Card } from "@mui/material";
+import { Autocomplete, Box, Card, Grid2, TextField, Typography } from "@mui/material";
 import React from "react";
 
-export const Addorder = ()=>{
-    return(
-        <Card></Card>
+export const Addorder = () => {
+    return (
+        <Card>
+            <Grid2 container spacing={3}>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+                <Grid2 size={4}>
+                    <Box sx={{ mb: 1 }}>
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            fontSize={{ xs: "12px", sm: "12px", md: "12px" }}
+                            color="#333333"
+                            fontWeight={600}
+                        >
+                            IPRD Reference Number
+                        </Typography>
+                        <Autocomplete renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                placeholder="Search..."
+                                variant="outlined"
+                                sx={{ fontSize: "14px" }}
+                            />
+                        )} />
+                    </Box>
+                </Grid2>
+            </Grid2>
+        </Card>
     )
 }
